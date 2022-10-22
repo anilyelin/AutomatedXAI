e404d5f0e3f704ad3fe30639d29a431757ef3f15
Anil <yelin.anil@gmail.com>
2022-10-22 18:22:51 +0200

    delete unnecesary code

--- streamlit_main.py
+++ streamlit_main.py
@@ -832,12 +832,4 @@ else:
     st.write("Conclusion: Black Box Model <ETC> has a higher total of weight of: ", np.round(etc_eli5_sum,4))
 
 
-import os
-def file_selector(folder_path='..'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
-
-filename = file_selector()
-st.write('You selected `%s`' % filename)
 
